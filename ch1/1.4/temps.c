2ce80dde4abda9b9cb5372b1846cd14d00bdbc45
Chris Grubb <cgrubb86@gmail.com>
2015-11-13 21:03:51 -0600

    Added new temps.c and Makefile

---
+++ ch1/1.4/temps.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define LOWER 0   /* Lower limit of table */
+#define UPPER 300 /* Upper limit of table */
+#define STEP 20   /* step size */
+
+/* print Fahrenheit-Celcius table */
+int main()
+{
+  return 0;
+}
