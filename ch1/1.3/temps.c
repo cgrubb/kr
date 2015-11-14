e10827b0c00fdefd84a90da96dc421b7649b0073
Chris Grubb <cgrubb86@gmail.com>
2015-11-13 20:56:31 -0600

    1. Added printing function 2. Added reverse temp list

--- ch1/1.3/temps.c
+++ ch1/1.3/temps.c
@@ -5,10 +5,20 @@ int main()
 {
   int fahr;
 
+  void printTemp(int temp)
+  {
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (temp - 32));
+  }
+
   for (fahr = 0; fahr <= 300; fahr = fahr + 20)
   {
-    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    printTemp(fahr);
   }
   
+  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+  {
+    printTemp(fahr);
+  }
+
   return 0;
 }
