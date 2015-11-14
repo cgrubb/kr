7cdb549c6a1a4495f8bff9e1ef63727acb9a00f6
Chris Grubb <cgrubb86@gmail.com>
2015-11-13 21:10:32 -0600

    More edits

--- ch1/1.4/temps.c
+++ ch1/1.4/temps.c
@@ -4,8 +4,15 @@
 #define UPPER 300 /* Upper limit of table */
 #define STEP 20   /* step size */
 
-/* print Fahrenheit-Celcius table */
+/* print Fahrenheit-Celsius table */
 int main()
 {
+  int fahr;
+
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+  {
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+  }
+
   return 0;
 }
